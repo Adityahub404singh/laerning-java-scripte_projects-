84cc3440e425ce050ea60b4d75c430ea1bdb4baa
Aditya Singh <singhaditya4560@gmail.com>
2026-07-01 12:13:09 +0530

    first commit

---
+++ prectic sets/index.js
@@ -0,0 +1,11 @@
+alert('welcom buddy');
+// Single element select karne ke liye (index 0 = first match)
+// its only in html chnges 
+document.getElementsByClassName("bag_button")[0].innerHTML = "Hello"; 
+//  console.log("hello");
+// its use to find elemnt 
+document.getElementsByClassName("bag_button");
+// its use to changes in css
+document.querySelector(".bag_button").innerHTML = "Hello";
+// it is use in console
+document.body.innerHTML="<p>hi its only check to chnage hole body<p>"
