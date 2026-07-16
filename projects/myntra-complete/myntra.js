3dd885a4b424fe82146a1867a43af62bd14bfc8a
Aditya Singh <singhaditya4560@gmail.com>
2026-07-17 04:21:54 +0530

    myntra funtioning upadates

---
+++ projects/myntra-complete/myntra.js
@@ -0,0 +1,21 @@
+let itemscontainerelement = document.querySelector(".items-container");
+
+
+
+
+
+
+
+
+itemscontainerelement.innerHTML = `<div class="item-container">
+                <img src="../../images/categories/9.jpg" alt="myntra item" class="item-image">
+                <div class="rating">4.5★|1.4k</div>
+                <div class="company-name">Carlton London</div>
+                <div class="item-name">Rhodium-plated CZ Floral Studs</div>
+                <div class="price">
+                    <span class="current-price">₹1,999</span>
+                    <span class="original-price">₹3,999</span>
+                    <span class="discount">(50% off)</span>
+                </div>
+                <button class="btn-add-Bag">Add to Bag</button>
+            </div>"`
